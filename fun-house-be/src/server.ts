91d3436efa7b4547caec2e69e8fbfe11caaa7a01
BarryLiu1995 <ssdf90771@gmail.com>
2019-07-16 15:14:42 +0800

    feat: 增加简单日志记录功能

--- fun-house-be/src/server.ts
+++ fun-house-be/src/server.ts
@@ -2,13 +2,19 @@ import {QueryType} from './model';
 import app from './app';
 import {Spider} from './spider';
 import express from 'express';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 const port = 8090;
 let spider = new Spider();
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
 
 app.listen(port, function () {
   console.log('Express server listening on port ' + port);
 });
+
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.get('/search', (req: express.Request, res: express.Response) => {
   let query: QueryType = req.query;
